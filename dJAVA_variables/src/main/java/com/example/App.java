cb9c68a40ea8b253b8f4d8ff692bcdd8b3c9f508
Tae Ngyee <tae@Ubuntu24.04-Swift-SF314-54>
2026-04-03 07:05:57 +0700

    03/04/26

---
+++ dJAVA_variables/src/main/java/com/example/App.java
@@ -0,0 +1,26 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        // basic datatypes
+        String name = "Nguyen Duc Tay";
+        int birthYear = 1999;
+        boolean isStraight = true;
+        char firstLetter = 'T';
+
+        // basic number datatypes
+        int a = 10;
+        double b = 10.5;
+        float c = 10.5f;
+        long d = 10L;
+
+        System.out.println("Full name: " + name);
+        System.out.println("Birth year: " + birthYear);
+        System.out.println("Is straight? " + isStraight);
+        System.out.println("Fistname letter: " + firstLetter);
+
+        
+    }
+}
