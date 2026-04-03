ce6b86b3e8f650858205fef739a838f4ca73b014
Tae Ngyee <tae@Ubuntu24.04-Swift-SF314-54>
2026-04-03 21:04:43 +0700

    03/04/26

--- dJAVA_variables/src/main/java/com/example/App.java
+++ dJAVA_variables/src/main/java/com/example/App.java
@@ -2,25 +2,32 @@ package com.example;
 
 public class App {
 
+    static int globalX = 10;
+
     public static void main(String[] args) {
 
-        // basic datatypes
         String name = "Nguyen Duc Tay";
         int birthYear = 1999;
         boolean isStraight = true;
         char firstLetter = 'T';
-
-        // basic number datatypes
-        int a = 10;
-        double b = 10.5;
-        float c = 10.5f;
-        long d = 10L;
 
         System.out.println("Full name: " + name);
         System.out.println("Birth year: " + birthYear);
         System.out.println("Is straight? " + isStraight);
         System.out.println("Fistname letter: " + firstLetter);
 
-        
+        double localX = 10.5;
+
+        if (isStraight = true) {
+
+            float blockX = 10.5f;
+
+            System.out.println(blockX);
+        }
+
+        System.out.println(globalX);
+        System.out.println(localX);
+        //System.out.println(blockX); error
+
     }
 }
